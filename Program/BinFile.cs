7a67fce52fdc73d71e9f88b4cee9e97f0c887d66
MAXXXYMIRON <klavioturasucka2@gmail.com>
2020-05-27 17:10:52 +0300

    binFile0

---
+++ Program/BinFile.cs
@@ -0,0 +1,220 @@
+﻿using System;
+using System.IO;
+namespace Program
+{
+    /// <summary>
+    /// У казывает на начальную, текущую или последнюю
+    /// позицию в файле, соответственно.
+    /// </summary>
+    public enum Seek { BEG, CUR, END }
+
+    class BinFile
+    {
+        /// <summary>
+        /// Перемення для записи в файл.
+        /// </summary>
+        BinaryWriter write;
+        /// <summary>
+        /// Переменная для чтения из файла.
+        /// </summary>
+        BinaryReader read;
+        /// <summary>
+        /// Размер записи.
+        /// </summary>
+        int size;
+
+
+        /// <summary>
+        /// Название файла.
+        /// </summary>
+        public readonly string FileName;
+
+        /// <summary>
+        /// Кол-во записей в файле.
+        /// </summary>
+        public long Length { get; private set; }
+
+        /// <summary>
+        /// Текущая позиция в файле.
+        /// </summary>
+        public long Position { get; private set; }
+
+
+        /// <summary>
+        /// Конструктор с заданием имени файла.
+        /// </summary>
+        public BinFile(string fileName)
+        {
+            FileName = fileName;
+            Length = 0;
+            Position = 0;
+            size = 4;
+        }
+
+        /// <summary>
+        /// Проверка условия конца файла.
+        /// </summary>
+        public bool EOF => (Position == Length);
+
+        /// <summary>
+        /// Установить позицию в файле относительно указанной позиции.
+        /// </summary>
+        public void Positioning(long pos, Seek seek = Seek.CUR)
+        {
+            if(seek == Seek.BEG)
+            {
+                if (pos > Length)
+                    throw new Exception("Позиция выходит за пределы файла.");
+                if (pos < 0)
+                    throw new Exception("Смещение не может быть отрицательным.");
+
+                Position = pos;
+            }
+            else if(seek == Seek.CUR)
+            {
+                if((pos + Position) >= Length || (pos + Position) < 0)
+                    throw new Exception("Позиция выходит за пределы файла.");
+
+                Position += pos;
+
+            }
+            else
+            {
+                if ((pos + Length) < 0)
+                    throw new Exception("Позиция выходит за пределы файла.");
+                if (pos > 0)
+                    throw new Exception("Смещение не может быть положительным.");
+
+                Position = Length + pos;
+            }
+        }
+
+        /// <summary>
+        /// Очистить файл.
+        /// </summary>
+        public void Clear()
+        {
+            using (write = new BinaryWriter(new FileStream(FileName, FileMode.Truncate))) { }
+            Length = 0;
+            Position = 0;
+        }
+
+        /// <summary>
+        /// Установить позицию в начало файла.
+        /// </summary>
+        public void Reset()
+        {
+            Position = 0;
+            using (write = new BinaryWriter(new FileStream(FileName, FileMode.OpenOrCreate)))
+            {
+                write.BaseStream.Position = Position; 
+            }
+        }
+
+
+
+        /// <summary>
+        /// Записать в файл данные data, начиная с position
+        /// по умолчанию position - это текущая позиция в файле.
+        /// </summary>
+        public void Write(int[] data, long position = -1)
+        {
+            if (position >= 0) Positioning(position, Seek.BEG);
+            using (write = new BinaryWriter(new FileStream(FileName, FileMode.OpenOrCreate)))
+            {
+                write.BaseStream.Position = Position * size;
+                for (int i = 0; i < data.Length; i++)
+                {
+                    write.Write(data[i]);
+                }
+                Position = write.BaseStream.Position / size;
+                Length = write.BaseStream.Length / size;
+            }
+        }
+
+        /// <summary>
+        /// Записать в файл данные data, начиная с position
+        /// по умолчанию position - это текущая позиция в файле.
+        /// </summary>
+        public void Write(int data, long position = -1)
+        {
+            if (position >= 0) Positioning(position, Seek.BEG);
+            using (write = new BinaryWriter(new FileStream(FileName, FileMode.OpenOrCreate)))
+            {
+                write.BaseStream.Position = Position * size;
+                write.Write(data);
+
+                Position = write.BaseStream.Position / size;
+                Length = write.BaseStream.Length / size;
+            }
+        }
+
+        /// <summary>
+        /// Читать данные, начиная с position
+        /// по умолчанию position - это текущая позиция в файле.
+        /// </summary>
+        public int[] Read(long n, long position = -1)
+        {
+            if(position >= 0) Positioning(position, Seek.BEG);
+
+            int[] data = new int[n];
+
+            using (read = new BinaryReader(new FileStream(FileName, FileMode.OpenOrCreate)))
+            {
+                read.BaseStream.Position = Position * size;
+
+                for (int i = 0; i < n; i++)
+                {
+                    data[i] = read.ReadInt32();
+                }
+
+                Position = read.BaseStream.Position / size;
+            }
+            return data;
+        }
+
+        /// <summary>
+        /// Читать данные начиная с текущей позиции в файле,
+        /// до конца.
+        /// </summary>
+        public int[] Read()
+        {
+            int[] data = new int[Length - Position];
+
+            using (read = new BinaryReader(new FileStream(FileName, FileMode.OpenOrCreate)))
+            {
+                read.BaseStream.Position = Position * size;
+
+                for (long i = 0; i < data.Length; i++)
+                {
+                    data[i] = read.ReadInt32();
+                }
+
+                Position = read.BaseStream.Position / size;
+            }
+
+            return data;
+        }
+
+        /// <summary>
+        /// Читать данные, начиная с position
+        /// по умолчанию position - это текущая позиция в файле.
+        /// </summary>
+        public int Read(long position = -1)
+        {
+            if (position >= 0) Positioning(position, Seek.BEG);
+
+            int data;
+
+            using (read = new BinaryReader(new FileStream(FileName, FileMode.OpenOrCreate)))
+            {
+                read.BaseStream.Position = Position * size;
+
+                data = read.ReadInt32();
+
+                Position = read.BaseStream.Position / size;
+            }
+            return data;
+        }
+    }
+}
---
+++ Program/Program.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.IO;
+using System.Diagnostics;
+
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random(); 
+
+            BinFile binFile = new BinFile(@"F:\Тест сортировки на внешних носителях\file.txt");
+
+            int[] data = new int[100];
+            Stopwatch time = new Stopwatch();
+
+            using (StreamWriter writer = new StreamWriter("Test.txt"))
+            {
+                //Рандомный массив
+                for (int i = 0; i < data.Length; i++)
+                {
+                    data[i] = rand.Next(0, 99);
+                    writer.Write(data[i] + "  ");
+                }
+                writer.Write("\n\nMergeSort\n");
+                //Пишем массив файл
+                binFile.Write(data);
+
+                //Сортируем файл вычисляя время сортировки
+                time.Start();
+                MergeSort(binFile, 10);
+                time.Stop();
+                
+                //Читаем отсортированный файл в массив
+                binFile.Reset();
+                data = binFile.Read();
+
+                //вывод результатов
+                for (int i = 0; i < data.Length; i++)
+                {
+                    writer.Write(data[i] + "  ");
+                }
+                writer.Write("\n\n" + time.ElapsedMilliseconds + " milli seconds");
+            }
+            binFile.Clear();
+        }
+
+        //Быстрая сортировка, для кусков файла
+        static void QuickSort(int[] a, int low, int high)
+        {
+            void Swap(int index1, int index2)
+            {
+                int t = a[index1];
+                a[index1] = a[index2];
+                a[index2] = t;
+            }
+
+            if (high - low <= 0) return;
+            else if (high - low == 1)
+            {
+                if (a[high] < a[low]) Swap(high, low);
+                return;
+            }
+
+            int up, down;
+            int middle = (high + low) / 2;
+            int center = a[middle];
+            Swap(middle, low);
+            up = low + 1;
+            down = high;
+
+            do
+            {
+                while (up <= down && a[up] <= center) up++;
+
+                while (a[down] > center) down--;
+
+                if (up < down) Swap(up, down);
+            }
+            while (up < down);
+
+            a[low] = a[down];
+            a[down] = center;
+
+
+            if (low < down - 1) QuickSort(a, low, down - 1);
+
+            if (down + 1 < high) QuickSort(a, down + 1, high);
+        }
+
+        //Сортировка естественным слиянием
+        static void MergeSort(BinFile fC, int blockSize)
+        {
+            //Буферы для чтения и сортировки блоков
+            int[] bufer1, bufer2;
+
+            fC.Reset();
+            //Файлы для сортировки
+            BinFile fA = new BinFile(@"F:\Тест сортировки на внешних носителях\fA.txt");
+            BinFile fB = new BinFile(@"F:\Тест сортировки на внешних носителях\fB.txt");
+            int use = 0;
+
+            //Читать блоки из файла fC пока его размер больше считываемого блока.
+            while (fC.Length > blockSize)
+            {
+                //Читать блоки поочередно в файлы fA и fB
+                while (!fC.EOF)
+                {
+                    if (!(fC.Position + blockSize > fC.Length))
+                        bufer1 = fC.Read(n: blockSize);
+                    else
+                        bufer1 = fC.Read();
+
+                    use++;
+                    QuickSort(bufer1, 0, bufer1.Length - 1);
+
+                    if (use % 2 == 1)
+                        fA.Write(bufer1);
+                    else
+                        fB.Write(bufer1);
+                }
+
+                fA.Reset();
+                fB.Reset();
+                fC.Clear();
+
+                //Записать блоки обратно в файл fC
+                while(true)
+                {
+                    bufer1 = null;
+                    bufer2 = null;
+
+                    if (!fA.EOF)
+                    {
+                        if (!(fA.Position + blockSize > fA.Length))
+                            bufer1 = fA.Read(n: blockSize);
+                        else
+                            bufer1 = fA.Read();
+                    }
+                    if (!fB.EOF)
+                    {
+                        if (!(fB.Position + blockSize > fB.Length))
+                            bufer2 = fB.Read(n: blockSize);
+                        else
+                            bufer2 = fB.Read();
+                    }
+
+                    if (bufer1 == null && bufer2 == null) break;
+
+                    if (bufer1 == null)
+                    {
+                        fC.Write(bufer2);
+                        continue;
+                    }
+                    if (bufer2 == null)
+                    {
+                        fC.Write(bufer1);
+                        continue;
+                    }
+
+                    fC.Write(bufer1);
+                    fC.Write(bufer2);
+                }
+
+                fA.Clear();
+                fB.Clear();
+                fC.Reset();
+                use = 0;
+                blockSize *= 2;
+            }
+
+            if (fC.Length <= blockSize)
+            {
+                bufer1 = fC.Read(fC.Length, 0);
+                QuickSort(bufer1, 0, bufer1.Length - 1);
+                fC.Write(bufer1, 0);
+                return;
+            }
+        }
+    }
+}
